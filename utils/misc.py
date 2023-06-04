666f9ae2ee67bf4da098dd777b5dcc65c52c9943
Tristan Saidi <tls2160@columbia.edu>
2023-06-04 13:08:13 -0400

    adding utils and config file

---
+++ utils/misc.py
@@ -0,0 +1,43 @@
+# from https://github.com/leggedrobotics/rsl_rl/blob/master/rsl_rl/algorithms/ppo.py
+
+class AdaptiveScheduler(object):
+    def __init__(self, kl_threshold=0.008):
+        super().__init__()
+        self.min_lr = 1e-6
+        self.max_lr = 1e-2
+        self.kl_threshold = kl_threshold
+
+    def update(self, current_lr, kl_dist):
+        lr = current_lr
+        if kl_dist > (2.0 * self.kl_threshold):
+            lr = max(current_lr / 1.5, self.min_lr)
+        if kl_dist < (0.5 * self.kl_threshold):
+            lr = min(current_lr * 1.5, self.max_lr)
+        return lr
+
+class AverageScalarMeter(object):
+    def __init__(self, window_size):
+        self.window_size = window_size
+        self.current_size = 0
+        self.mean = 0
+
+    def update(self, values):
+        size = values.size()[0]
+        if size == 0:
+            return
+        new_mean = torch.mean(values.float(), dim=0).cpu().numpy().item()
+        size = np.clip(size, 0, self.window_size)
+        old_size = min(self.window_size - size, self.current_size)
+        size_sum = old_size + size
+        self.current_size = size_sum
+        self.mean = (self.mean * old_size + new_mean * size) / size_sum
+
+    def clear(self):
+        self.current_size = 0
+        self.mean = 0
+
+    def __len__(self):
+        return self.current_size
+
+    def get_mean(self):
+        return self.mean
