7969a521c9bfe9c518346bd3516c60b5ec97d2b4
Tristan Saidi <tls2160@columbia.edu>
2023-06-04 13:04:26 -0400

    PPO module skeleton set up and instantiated in main.py

--- algo/ppo.py
+++ algo/ppo.py
@@ -1,10 +1,8 @@
 # --------------------------------------------------------
 # Tristan Saidi's PPO Implementation
 # PPO base class
-# Adapted from rlgames implementation of PPO
+# Adapted from Haozhi Qi's implementation of PPO
 # --------------------------------------------------------
-
-import interfaces.BaseAlgorithm
 
 import copy
 import os
@@ -12,13 +10,95 @@ import numpy as np
 import time
 import gym
 import torch
+from tensorboardX import SummaryWriter
 
-class PPO(BaseAlgorithm):
+from networks.running_mean_std import RunningMeanStd
+from utils.misc import AdaptiveScheduler, AverageScalarMeter
+from utils.replay_buffer import ReplayBuffer
+from networks.model import ActorCritic
+
+class PPO(object):
     def __init__(self, env, config):
-        pass
+        self.device = config["device"]
+        self.build_environment(env, config)
+        self.init_model(config)
+        self.configure_output_dir(config["experiment"])
+        self.configure_optimizer(config["ppo"])
+        self.configure_scheduler(config["ppo"])
+        self.set_ppo_train_params(config["ppo"])
+        self.set_ppo_rollout_params(config["ppo"])
+        self.configure_saving(config["ppo"])
+        self.episode_rewards = AverageScalarMeter(100)
+        self.episode_lengths = AverageScalarMeter(100)
+        self.obs = None
+        self.epoch_num = 0
 
-    def device(self):
-        pass
+    def build_environment(self, env, config):
+        self.env = env
+        self.num_actors = config["ppo"]["num_actors"]
+        action_space = self.env.action_space
+        self.actions_num = action_space.shape[0]
+        self.actions_low = torch.from_numpy(action_space.low.copy()).float().to(self.device)
+        self.actions_high = torch.from_numpy(action_space.high.copy()).float().to(self.device)
+        self.observation_space = self.env.observation_space
+        self.obs_shape = self.observation_space.shape[0]
+
+    def init_model(self, config):
+        args = {
+            "actor_units" : config["network"]["mlp"]["units"],
+            "critic_units" : config["network"]["mlp"]["units"],
+            "num_actions" : self.actions_num,
+            "input_shape" : self.obs_shape,
+        }
+        self.model = ActorCritic(args)
+        self.model.to(self.device)
+        self.running_mean_std = RunningMeanStd(self.obs_shape).to(self.device)
+        self.value_mean_std = RunningMeanStd((1,)).to(self.device)
+
+    def configure_output_dir(self, output_dir):
+        self.output_nn = os.path.join(output_dir, "nn")
+        self.output_tb = os.path.join(output_dir, "tb")
+        os.makedirs(self.output_nn, exist_ok = True)
+        os.makedirs(self.output_tb, exist_ok = True)
+
+    def configure_optimizer(self, ppo_config):
+        self.last_lr = float(ppo_config["learning_rate"])
+        self.weight_decay = 0.0
+        self.optimizer = torch.optim.Adam(self.model.parameters(), self.last_lr, weight_decay=self.weight_decay)
+        
+    def configure_scheduler(self, ppo_config):
+        self.kl_threshold = ppo_config["kl_threshold"]
+        self.scheduler = AdaptiveScheduler(self.kl_threshold)
+    
+    def set_ppo_train_params(self, ppo_config):
+        self.e_clip = ppo_config["e_clip"]
+        self.clip_value = ppo_config["clip_value"]
+        self.entropy_coef = ppo_config["entropy_coef"]
+        self.critic_coef = ppo_config["critic_coef"]
+        self.bounds_loss_coef = ppo_config["bounds_loss_coef"]
+        self.gamma = ppo_config["gamma"]
+        self.tau = ppo_config["tau"]
+        self.truncate_grads = ppo_config["truncate_grads"]
+        self.grad_norm = ppo_config["grad_norm"]
+        self.value_bootstrap = ppo_config["value_bootstrap"]
+        self.normalize_advantage = ppo_config["normalize_advantage"]
+        self.normalize_input = ppo_config["normalize_input"]
+        self.normalize_value = ppo_config["normalize_value"]
+
+    def set_ppo_rollout_params(self, ppo_config):
+        self.horizon_length = ppo_config["horizon_length"]
+        self.batch_size = self.horizon_length * self.num_actors
+        self.minibatch_size = ppo_config["minibatch_size"]
+        self.mini_epochs_num = ppo_config["mini_epochs"]
+        assert self.batch_size % self.minibatch_size == 0
+
+    def configure_saving(self, ppo_config):
+        self.save_freq = ppo_config["save_frequency"]
+        self.save_best_after = ppo_config["save_best_after"]
+
+    def configure_tensorboard(self):
+        self.extra_info = {}
+        self.writer = SummaryWriter(self.output_tb)
 
     def clear_stats(self):
         pass
--- main.py
+++ main.py
@@ -6,21 +6,50 @@
 
 import argparse
 import gym
+import os
+import numpy as np
 import sys
 import torch
+import hydra
+import random
+from omegaconf import DictConfig, OmegaConf
 
-# from ppo import PPO
-from networks.model import ActorCritic
+from algo.ppo import PPO
 
-def test_agent(env, checkpoint):
-    print("test")
+OmegaConf.register_new_resolver('eq', lambda x, y: x.lower() == y.lower())
+OmegaConf.register_new_resolver('contains', lambda x, y: x.lower() in y.lower())
+OmegaConf.register_new_resolver('if', lambda pred, a, b: a if pred else b)
+OmegaConf.register_new_resolver('resolve_default', lambda default, arg: default if arg == '' else arg)
+
+
+def init_actor_critic(env, units):
+    input_shape = env.observation_space.shape[0]
+    num_actions = env.action_space.shape[0]
     args = {
-        "actor_units" : [3, 10, 3],
-        "critic_units" : [3, 10, 3],
-        "num_actions" : 1,
-        "input_shape" : 3
+        "actor_units" : units,
+        "critic_units" : units,
+        "num_actions" : num_actions,
+        "input_shape" : input_shape,
     }
-    policy = ActorCritic(args)
+    return ActorCritic(args)
+
+
+# from https://github.com/HaozhiQi/hora
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+    return seed
+
+def test_agent(env, ppo_config):
+    policy = PPO(env, ppo_config)
 
     obs, _ = env.reset()
 
@@ -35,10 +64,11 @@ def test_agent(env, checkpoint):
     
     env.close()
 
-def train(env, checkpoint):
+def train(env, output_dir, ppo_config):
     pass
 
-def main()->int:
+@hydra.main(config_name='ppo', config_path='configs')
+def main(ppo_config: DictConfig)->int:
 
     parser =  argparse.ArgumentParser(
         'Simulate agent in a gym environment'
@@ -57,17 +87,34 @@ def main()->int:
     )
 
     parser.add_argument(
+        '--experiment',
+        default = 'outputs/ppo_agent',
+        type = str
+    )
+
+    parser.add_argument(
         '--env',
         default = 'Pendulum-v1',
         type = str
     )
 
-    args = parser.parse_args()
-    configs = args.__dict__
-
-    env = gym.make(configs['env'], render_mode='human')
+    command_args = parser.parse_args()
+    command_configs = command_args.__dict__
     
-    test_agent(env, configs["checkpoint"])
+    # Copy command args to ppo_config dict
+    ppo_config = OmegaConf.to_container(ppo_config)
+    ppo_config["checkpoint"] = command_configs["checkpoint"]
+    ppo_config["seed"] = set_seed(ppo_config["seed"])
+    ppo_config["test"] = command_configs["test"]
+    ppo_config["checkpoint"] = command_configs["checkpoint"]
+    ppo_config["experiment"] = command_configs["experiment"]
+
+    env = gym.make(command_configs['env'], render_mode='human')
+
+    if command_configs["test"]:
+        test_agent(env, ppo_config)
+    else:
+        train_agent(env, ppo_config)
 
     return 0
 
